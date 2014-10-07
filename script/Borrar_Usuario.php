6ef2123613b3da846e0017d07f03f49569acbb23
stipelson <stipelson@gmail.com>
2014-10-06 19:41:41 -0500

    para ser revisado

--- script/Borrar_Usuario.php
+++ script/Borrar_Usuario.php
@@ -1,18 +1,9 @@
 <?php
-	include ("../pages/perfil.php"); 
+	include_once '../class/Validacion_Usuario.php'; 
 
-	$doc = $_REQUEST['documento'];
 
-		if($c_perfil->get_PermisoPerfiles()){
-
-				if($m_usuario->eliminar_Usuario($doc)){
-					header("Location: ../pages/Eliminar_Usuario.php?gestion=exito");
-				}else{
-					header("Location: ../pages/Eliminar_Usuario.php?gestion=error");
-				}
-	 		
-	 	}else
-			echo "<h1><i>Esto no te pertenece.</i></h1>";
+	$borrar_usuario = new Validar_Usuario();
+	$borrar_usuario->borrar_Usuario($_REQUEST['documento']);	
 
 
 ?>
